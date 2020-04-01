d6cfa31d6b593149d4d310aaedd9257ea4f1ce79
voltamage <37029882+voltamage@users.noreply.github.com>
2020-03-31 23:37:17 -0700

    Update aliases.zsh

--- armbian/.oh-my-zsh/custom/aliases.zsh
+++ armbian/.oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,5 @@
 alias aliases='vim ~/.oh-my-zsh/custom/aliases.zsh && sort -n ~/.oh-my-zsh/custom/aliases.zsh > /tmp/aliases.zsh && mv /tmp/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh && exec zsh'
 alias diskhealth='sudo smartctl -i /dev/sda && sudo smartctl -A /dev/sda'
 alias iplist='nmap -sP 192.168.2.1/24'
+alias mntdu='sudo ncdu /mnt'
+alias sysdu='sudo ncdu / --exclude /mnt'
