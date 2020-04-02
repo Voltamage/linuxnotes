420357090d0619f56f230ce73df1d409b371f150
voltamage <37029882+voltamage@users.noreply.github.com>
2020-04-01 23:10:03 -0700

    Update aliases.zsh

--- armbian/.oh-my-zsh/custom/aliases.zsh
+++ armbian/.oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,5 @@
 alias diskhealth='sudo smartctl -i /dev/sda && sudo smartctl -A /dev/sda'
+alias hostsupdate='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/notes/master/armbian/tmp/hosts_updater)"'
 alias iplist='nmap -sP 192.168.2.1/24'
 alias mntdu='sudo ncdu /mnt'
 alias patch='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/notes/master/armbian/tmp/patcher)"'
